ee951648334aeb00869ef1e648b195a3a738a5d3
Isha C <ishac1017@gmail.com>
2026-02-28 14:44:14 -0600

    changed the file types

--- smart-park-web/src/App.jsx
+++ smart-park-web/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth0ProviderWrapper from "./auth/auth0-provider";
+import NavBar from "./components/navBar";
+import Home from "./pages/home";
+import ParkingDetails from "./pages/parkingDetails";
+import Dashboard from "./pages/dashboard";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth0();
+  return isAuthenticated ? children : <div>Please log in to view this page.</div>;
 }
 
-export default App
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Auth0ProviderWrapper>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/parking/:id" element={<ParkingDetails />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Auth0ProviderWrapper>
+    </BrowserRouter>
+  );
+}
